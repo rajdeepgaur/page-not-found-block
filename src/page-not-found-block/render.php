8985b51bad15a82cecbe66eb89ef8f840fdc2827
Rajdeep Gaur <gaur.rajdeep95@gmail.com>
2025-02-20 20:21:13 +0530

    added all the possible conditions for prefix and suffix in render.php

--- src/page-not-found-block/render.php
+++ src/page-not-found-block/render.php
@@ -14,21 +14,93 @@ if (!function_exists('extractLastPart')){
 
 $last_part = extractLastPart($_SERVER['REQUEST_URI']);
 
-if ( ! empty( $attributes['prefix'] ) && ! empty( $attributes['showPrefix'] ) && ! empty($attributes['suffix'] ) && ! empty( $attributes['showSuffix'] )) {
+
+// Default value
+$display_text = $last_part;
+
+// Case 1: Both prefix & suffix exist and should be shown
+if (
+    !empty($attributes['prefix']) && 
+    !empty($attributes['showPrefix']) && 
+    !empty($attributes['suffix']) && 
+    !empty($attributes['showSuffix'])
+) {
     $display_text = $attributes['prefix'] . '"' . $last_part . '"' . $attributes['suffix'];
-} else {
-    $display_text = $last_part;
 }
 
-if ( ! empty( $attributes['prefix'] ) && ! empty( $attributes['showPrefix'] ) &&  empty($attributes['suffix'] ) && empty( $attributes['showSuffix'] )) {
+// Case 2: Show suffix is enabled, but suffix is empty → Only prefix + last_part
+elseif (
+    !empty($attributes['showSuffix']) && 
+    empty($attributes['suffix']) && 
+    !empty($attributes['prefix']) && 
+    !empty($attributes['showPrefix'])
+) {
     $display_text = $attributes['prefix'] . '"' . $last_part . '"';
-} else {
+}
+
+// Case 3: Show prefix is enabled, but prefix is empty → Only last_part + suffix
+elseif (
+    !empty($attributes['showPrefix']) && 
+    empty($attributes['prefix']) && 
+    !empty($attributes['suffix']) && 
+    !empty($attributes['showSuffix'])
+) {
+    $display_text = '"' . $last_part . '"' . $attributes['suffix'];
+}
+
+// Case 4: Prefix and showPrefix are empty, but suffix and showSuffix exist → Show only last_part + suffix
+elseif (
+    empty($attributes['prefix']) && 
+    empty($attributes['showPrefix']) && 
+    !empty($attributes['suffix']) && 
+    !empty($attributes['showSuffix'])
+) {
+    $display_text = '"' . $last_part . '"' . $attributes['suffix'];
+}
+
+// Case 5: Suffix and showSuffix are empty, but prefix and showPrefix exist → Show only prefix + last_part
+elseif (
+    !empty($attributes['prefix']) && 
+    !empty($attributes['showPrefix']) && 
+    empty($attributes['suffix']) && 
+    empty($attributes['showSuffix'])
+) {
+    $display_text = $attributes['prefix'] . '"' . $last_part . '"';
+}
+
+// Case 6: Only showPrefix & showSuffix are set, but prefix and suffix are empty → Show only last_part
+elseif (
+    !empty($attributes['showPrefix']) && 
+    empty($attributes['prefix']) && 
+    !empty($attributes['showSuffix']) && 
+    empty($attributes['suffix'])
+) {
     $display_text = $last_part;
 }
 
-if ( empty( $attributes['prefix'] ) && empty( $attributes['showPrefix'] ) && ! empty($attributes['suffix'] ) && ! empty( $attributes['showSuffix'] )) {
-    $display_text = '"' . $last_part . '"' . $attributes['suffix'];
-} else {
+// Case 7: Both showPrefix & showSuffix are empty → Show only last_part
+elseif (
+    empty($attributes['showPrefix']) && 
+    empty($attributes['showSuffix'])
+) {
+    $display_text = $last_part;
+}
+
+// Case 8: Suffix should be shown but is empty, and showPrefix is empty → Show only last_part
+elseif (
+    !empty($attributes['showSuffix']) && 
+    empty($attributes['suffix']) && 
+    empty($attributes['showPrefix'])
+) {
+    $display_text = $last_part;
+}
+
+// Case 9: Prefix should be shown but is empty, and showSuffix is empty → Show only last_part
+elseif (
+    !empty($attributes['showPrefix']) && 
+    empty($attributes['prefix']) && 
+    empty($attributes['showSuffix'])
+) {
     $display_text = $last_part;
 }
 
