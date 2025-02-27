fd6096751d5d7bf8e7f4e70f183d1a60e9cfa9a5
Rajdeep Gaur <gaur.rajdeep95@gmail.com>
2025-02-27 19:38:26 +0530

    Altered file structure

--- page-not-found-block.php
+++ page-not-found-block.php
@@ -25,6 +25,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function create_block_page_not_found_block_block_init() {
-	register_block_type( __DIR__ . '/build/page-not-found-block' );
+	register_block_type( __DIR__ . '/build' );
 }
 add_action( 'init', 'create_block_page_not_found_block_block_init' );
